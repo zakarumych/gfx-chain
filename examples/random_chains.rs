130ca9c0354a699c5eccb3c5d49f8fca4de6a08d
Lymia Aluysia <lymia@lymiahugs.com>
2018-04-17 06:49:07 -0500

    Improvements to fuzzer, to find minimal examples.

--- examples/random_chains.rs
+++ examples/random_chains.rs
@@ -6,18 +6,21 @@ extern crate rand;
 use clap::{Arg, App, SubCommand};
 use gfx_chain::build;
 use gfx_chain::pass::{Pass, PassId};
-use gfx_chain::resource::{Buffer, BufferLayout, State, Id, Image};
+use gfx_chain::resource::{Buffer, BufferLayout, State, Id, Image, Resource};
 use hal::buffer::{Access as BufferAccess};
 use hal::image::{Access as ImageAccess, Layout as ImageLayout};
 use hal::pso::PipelineStage;
 use hal::queue::QueueFamilyId;
 use rand::{Rng, SeedableRng, OsRng, Isaac64Rng};
 use std::cmp::min;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::panic::{catch_unwind, set_hook, AssertUnwindSafe};
 
 type DefaultRng = Isaac64Rng;
 
+fn gen_bool(rng: &mut DefaultRng) -> bool {
+    rng.gen_range(0, 2) == 0
+}
 fn gen_inclusive_u32(rng: &mut DefaultRng, low: u32, high: u32) -> u32 {
     rng.gen_range(low, high + 1)
 }
@@ -25,7 +28,7 @@ fn gen_inclusive(rng: &mut DefaultRng, low: usize, high: usize) -> usize {
     rng.gen_range(low, high + 1)
 }
 fn gen_inclusive_prefer_low(rng: &mut DefaultRng, low: usize, high: usize) -> usize {
-    if rng.gen_range(0, 2) == 0 {
+    if gen_bool(rng) {
         rng.gen_range(low, high + 1)
     } else {
         low
@@ -144,23 +147,36 @@ fn create_deps(rng: &mut DefaultRng, i: usize) -> Vec<PassId> {
     deps
 }
 
-fn create_buffer_deps(rng: &mut DefaultRng, count: u32) -> HashMap<Id<Buffer>, State<Buffer>> {
+fn create_resc_deps<R: Resource, F: Fn(&mut DefaultRng) -> State<R>>(
+    rng: &mut DefaultRng, count: u32, used: &mut HashSet<Id<R>>, new_state: F,
+) -> HashMap<Id<R>, State<R>> {
     let mut map = HashMap::new();
-    for _ in 0..gen_inclusive_u32(rng, 1, min(count, 4)) {
-        map.insert(Id::new(rng.gen_range(0, count)), create_buffer_state(rng));
-    }
-    map
-}
-fn create_image_deps(rng: &mut DefaultRng, count: u32) -> HashMap<Id<Image>, State<Image>> {
-    let mut map = HashMap::new();
-    for _ in 0..gen_inclusive_u32(rng, 1, min(count, 4)) {
-        map.insert(Id::new(rng.gen_range(0, count)), create_image_state(rng));
+    if count != 0 {
+        for _ in 0..gen_inclusive_u32(rng, 1, min(count, 4)) {
+            let id = Id::new(rng.gen_range(0, count));
+            used.insert(id);
+            map.insert(id, new_state(rng));
+        }
     }
     map
 }
 
-fn test_run(seed: u64, debug_print: bool) {
-    if debug_print {
+static mut PANIC_INFO: Option<Option<String>> = None;
+fn install_fuzz_panic_hook() {
+    set_hook(Box::new(|panic_info| {
+        if let Some(location) = panic_info.location() {
+            unsafe { PANIC_INFO = Some(Some(format!("{}:{}", location.file(), location.line()))) }
+        } else {
+            unsafe { PANIC_INFO = Some(None) }
+        }
+    }));
+}
+fn take_panic_info() -> Option<Option<String>> {
+    unsafe { PANIC_INFO.take() }
+}
+
+fn test_run(seed: u64, is_test: bool) -> usize {
+    if is_test {
         println!("Seed: {}", seed);
     }
 
@@ -168,53 +184,75 @@ fn test_run(seed: u64, debug_print: bool) {
     rng.reseed(&[seed]);
     let rng = &mut rng;
 
-    let queue_count = gen_inclusive_prefer_low(rng, 1, 4);
-    let buffer_count = gen_inclusive_u32(rng, 1, 10);
-    let image_count = gen_inclusive_u32(rng, 1, 10);
-    let submit_count = gen_inclusive(rng, 10, 30);
-    if debug_print {
-        println!("Creating test case with {} queues, {} buffers, {} images, and {} submissions.",
-                 queue_count, buffer_count, image_count, submit_count);
+    let family_count = gen_inclusive_prefer_low(rng, 1, 4);
+    let buffer_count = gen_inclusive_u32(rng, 0, 10);
+    let image_count = gen_inclusive_u32(rng, 0, 10);
+    let submit_count = gen_inclusive(rng, 1, 25);
+    if is_test {
+        println!("Creating test case with {} families, {} buffers, {} images, and {} submissions.",
+                 family_count, buffer_count, image_count, submit_count);
     }
 
     let mut max_queues = Vec::new();
-    for _ in 0..queue_count {
+    for _ in 0..family_count {
         max_queues.push(gen_inclusive_prefer_low(rng, 1, 5));
     }
-    if debug_print {
+    if is_test {
         println!("Max queues for families: {:?}", max_queues);
     }
 
+    let mut used_families = HashSet::new();
+    let mut used_buffers = HashSet::new();
+    let mut used_images = HashSet::new();
+
     let mut passes = Vec::new();
+    let mut pass_complexity = 0;
     for i in 0..submit_count {
+        let family = QueueFamilyId(rng.gen_range(0, family_count));
+        let queue = if gen_bool(rng) { Some(rng.gen_range(0, max_queues[family.0])) } else { None };
+        let dependencies = create_deps(rng, i);
+        let buffers = create_resc_deps(rng, buffer_count, &mut used_buffers, create_buffer_state);
+        let images = create_resc_deps(rng, image_count, &mut used_images, create_image_state);
+
+        used_families.insert(family);
+        if queue.is_some() { pass_complexity += 1; }
+        pass_complexity += dependencies.len() + buffers.len() + images.len();
+
         passes.push(Pass {
-            id: PassId(i),
-            family: QueueFamilyId(rng.gen_range(0, queue_count)),
-            queue: None,
-            dependencies: create_deps(rng, i),
-            buffers: create_buffer_deps(rng, buffer_count),
-            images: create_image_deps(rng, image_count),
+            id: PassId(i), family, queue, dependencies, buffers, images,
         })
     }
-    if debug_print {
+    if is_test {
         println!("Submissions: {:#?}", passes);
     }
+    rng.shuffle(&mut passes);
 
-    let mut semaphore_id = 0;
-    let chains = build(
-        passes,
-        |QueueFamilyId(id)| max_queues[id],
-        || {
-            let id = semaphore_id;
-            semaphore_id += 1;
-            (id, id)
+    catch_unwind(AssertUnwindSafe(|| {
+        let mut semaphore_id = 0;
+        let chains = build(
+            passes,
+            |QueueFamilyId(id)| max_queues[id],
+            || {
+                let id = semaphore_id;
+                semaphore_id += 1;
+                (id, id)
+            }
+        );
+
+        if is_test {
+            println!("Schedule: {:#?}", chains.schedule);
+            println!("Semaphore count: {}", semaphore_id);
         }
-    );
+    })).ok();
 
-    if debug_print {
-        println!("Schedule: {:#?}", chains.schedule);
-        println!("Semaphore count: {}", semaphore_id);
+    let mut queue_count = 0;
+    for &family in &used_families {
+        queue_count += max_queues[family.0];
     }
+
+    let queue_score = (used_families.len() - 1) * 10 + (queue_count - 1) as usize;
+    let object_score = submit_count + used_buffers.len() + used_images.len();
+    queue_score * 100 + object_score * 10 + pass_complexity
 }
 
 fn main() {
@@ -235,24 +273,40 @@ fn main() {
                 let seed = seed.parse::<u64>().expect("seed is not a number!");
                 test_run(seed, true);
             }
-            None => test_run(os_rng.next_u64(), true),
+            None => {
+                test_run(os_rng.next_u64(), true);
+            }
         }
         return
     }
     if let Some(_) = matches.subcommand_matches("fuzz") {
-        static mut SEED: u64 = 0;
-        set_hook(Box::new(|panic_info| {
-            if let Some(location) = panic_info.location() {
-                println!("Panic occurred: seed = {}, location = {}:{}",
-                         unsafe { SEED }, location.file(), location.line());
-            } else {
-                println!("Panic occurred: seed = {}, location = <unknown>", unsafe { SEED });
-            }
-        }));
+        install_fuzz_panic_hook();
+        let mut simplest_examples = HashMap::new();
         loop {
             let seed = os_rng.next_u64();
-            unsafe { SEED = seed; }
-            catch_unwind(AssertUnwindSafe(|| test_run(seed, false))).ok();
+            let example_complexity = test_run(seed, false);
+            match take_panic_info() {
+                Some(Some(location)) => {
+                    if simplest_examples.contains_key(&location) {
+                        let entry = simplest_examples.get_mut(&location).unwrap();
+                        if example_complexity < *entry {
+                            println!("Simpler example found for panic at {}. \
+                                      Seed = {}, Example complexity = {}",
+                                     location, seed, example_complexity);
+                            *entry = example_complexity;
+                        }
+                    } else {
+                        println!("/!\\ New panic found at {}. Seed = {}, Example complexity = {}",
+                                 location, seed, example_complexity);
+                        simplest_examples.insert(location, example_complexity);
+                    }
+                }
+                Some(None) => {
+                    println!("/!\\ New panic found at unknown location. Seed = {}, \
+                              Example complexity = {}", seed, example_complexity);
+                }
+                None => { }
+            }
         }
     }
     app.print_help().unwrap();
